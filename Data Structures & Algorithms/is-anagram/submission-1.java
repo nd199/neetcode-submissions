78560029165d4cb9ed50cc047dda9f9bb90cbd83
Narendran M D <naren06251999@gmail.com>
2026-06-16 18:55:19 +0530

    Update: is-anagram - submission-1

--- Data Structures & Algorithms/is-anagram/submission-1.java
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -1,14 +1,7 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        int[] charCount = new int[128];
-        for(char c : s.toCharArray())
-            charCount[c]++;
-
-        for(char c : t.toCharArray()) 
-            if(charCount[c] >= 1) 
-                charCount[c]--;
-            else return false;
-        return true;
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) if(!set.add(num)) return true;
+        return false;
     }
 }
